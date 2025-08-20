a505ad30c3260336cb59c29e2b63b7211dc6649b
Frank <fcarpio113@gmail.com>
2025-08-20 17:37:27 -0500

    First commit

---
+++ notification-service/src/test/java/org/carpio/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carpio.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
